b27a399539978afc10d12fc3bbd6da09717734cb
hebersedanodev <heberthsed@gmail.com>
2025-09-13 18:56:43 -0500

    Mejora finalizada de preguntas frecuentes

--- proyectofinal/src/main/resources/static/js/fragmentos.js
+++ proyectofinal/src/main/resources/static/js/fragmentos.js
@@ -6,3 +6,30 @@ document.addEventListener("DOMContentLoaded", () => {
         menu.classList.toggle("active");
     });
 });
+
+
+
+/*INICIO - FAQ*/
+
+document.addEventListener('DOMContentLoaded', function() {
+    const faqItems = document.querySelectorAll('.faq-item');
+
+    faqItems.forEach(item => {
+        const details = item.querySelector('details');
+        const summary = item.querySelector('.faq-question');
+
+        // Verificar estado inicial
+        if (details.open) {
+            summary.classList.add('active');
+        }
+
+        // Observar cambios en el estado
+        details.addEventListener('toggle', function() {
+            if (details.open) {
+                summary.classList.add('active');
+            } else {
+                summary.classList.remove('active');
+            }
+        });
+    });
+});
