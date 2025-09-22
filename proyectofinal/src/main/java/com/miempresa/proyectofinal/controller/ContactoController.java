bb1b5d8e5b9c7f8bdf5df6ea29acfa2b6dc562b0
hebersedanodev <heberthsed@gmail.com>
2025-09-21 21:21:30 -0500

    Direccion de Mensajes en el header solo disponible para administradores

--- proyectofinal/src/main/java/com/miempresa/proyectofinal/controller/ContactoController.java
+++ proyectofinal/src/main/java/com/miempresa/proyectofinal/controller/ContactoController.java
@@ -1,4 +1,7 @@
 package com.miempresa.proyectofinal.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/admin/mensajes")
 public class ContactoController {
 }
